d67110826e2502e5b32af1d26e104dfbf713b4ae
MatthewC <cuey@outlook.com>
2023-03-16 12:36:50 +1100

    added project and firebase

---
+++ src/config/Config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyA17pOcZOJkDBHjDw9aYf55c5mkocBsc64", 
+    authDomain: "classprojectaa.firebaseapp.com",
+    projectId: "classprojectaa", 
+    storageBucket: "classprojectaa.appspot.com",  
+    messagingSenderId: "221757511415", 
+    appId: "1:221757511415:web:0f3f6efc0fd9f923e1e5f2", 
+    measurementId: "G-TD39K7J7CR"
+};
+  
