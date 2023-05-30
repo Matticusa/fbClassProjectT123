6b4c6b8c6bd70a8a46ba9af19fe60f9c01910926
MatthewC <cuey@outlook.com>
2023-05-30 21:16:07 +1000

    More stuff

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,8 +1,38 @@
+import Form from "react-bootstrap/Form"
+import Container from "react-bootstrap/Container"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
+import Button from "react-bootstrap/Button"
+
 export function Signup () {
     return (
-        <div>
-            <h1>Sign up for an Account</h1>
-        </div>
+        <Container fluid className="mt-4">
+            <Row>
+                <Col className="head" md={{span: 4, offset: 4}}>
+                <form>
+                    <h3>Sign Up for an Account</h3>
+                    <Form.Group>
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control type="email" placeholder="you@domain.com"
+                        onChange={(evt) => console.log(evt.target.value) }
+                        />
+
+                    </Form.Group>
+
+                    <Form.Group>
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="minimum 8 characters"/>
+                        
+                    </Form.Group>
+                    <Button variant="primary" type="submit" className="my-2 w-100" size="lg">
+                        Sign Up
+                    </Button>
+
+                </form>
+                </Col>
+            </Row>
+
+        </Container>
         
     )
 }
